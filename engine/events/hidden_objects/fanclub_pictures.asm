0d8ac2b626b8d48ca1572eb6f4f50a9f38274ce4
UserWest <WestofMystic@gmail.com>
2021-10-29 19:44:49 -0400

    Added r/b text for pokemon fan club pictures

--- engine/events/hidden_objects/fanclub_pictures.asm
+++ engine/events/hidden_objects/fanclub_pictures.asm
@@ -1,23 +1,44 @@
 FanClubPicture1:
+	call CheckForYellowVersion
+	jr nz, .listenPolitely
 	ld a, RAPIDASH
 	ld [wcf91], a
 	call DisplayMonFrontSpriteInBox
 	call EnableAutoTextBoxDrawing
 	tx_pre FanClubPicture1Text
 	ret
-
+.listenPolitely
+	call EnableAutoTextBoxDrawing
+	tx_pre LetsListenPolitelyText
+	ret
+	
 FanClubPicture1Text::
 	text_far _FanClubPicture1Text
 	text_end
 
+LetsListenPolitelyText::
+	text_far _LetsListenPolitelyText
+	text_end
+
 FanClubPicture2:
+	call CheckForYellowVersion
+	jr nz, .bragRightBack
 	ld a, FEAROW
 	ld [wcf91], a
 	call DisplayMonFrontSpriteInBox
 	call EnableAutoTextBoxDrawing
 	tx_pre FanClubPicture2Text
 	ret
+.bragRightBack
+	call EnableAutoTextBoxDrawing
+	tx_pre BragRightBackText
+	ret
 
 FanClubPicture2Text::
 	text_far _FanClubPicture2Text
 	text_end
+
+BragRightBackText::
+	text_far _BragRightBackText
+	text_end
+	
