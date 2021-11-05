4c2d8cf7247d40b70aff9d12047e43cd3a74346d
UserWest <WestofMystic@gmail.com>
2021-11-05 11:24:44 -0400

    Repeat calls so that sprites appear in the correct locations after player movement

--- home/reload_sprites.asm
+++ home/reload_sprites.asm
@@ -18,20 +18,19 @@ ReloadMapSpriteTilePatterns::
 	call LoadFontTilePatterns
 	jp UpdateSprites
 
-LoadMapAfterVersionChange::
+LoadMapAfterVersionChange:: ; this really needs to be broken down into just the relevant code for speed
 	ldh a, [hLoadedROMBank]
 	push af
 	call DisableLCD
 	call ResetMapVariables
 	call LoadTextBoxTilePatterns
-	ld a, 69 ; load wUniversalVariable with a special value to leave
-	ld [wUniversalVariable], a	; LoadTilesetHeader before player is moved
-	call LoadMapHeader
-	call ClearVariable ; Resets the variable so it doesn't affect future loads
+	ld a, 69 					 ; load wUniversalVariable with a special value so we
+	ld [wUniversalVariable], a	 ; can leave LoadTilesetHeader before the player is moved
 	
-	ld hl, wFontLoaded  ; the important of ReloadMapSpriteTilePatterns
+	call LoadMapHeader 			 	
+	ld hl, wFontLoaded  ; this section is needed to reload the sprite sets
 	ld a, [hl]			; it could be its own function, but nothing else will
-	push af				; likely ever call it
+	push af				; likely ever call it, copy/pasted from ReloadMapSpriteTilePatterns
 	res 0, [hl]
 	push hl
 	xor a
@@ -40,10 +39,18 @@ LoadMapAfterVersionChange::
 	pop hl
 	pop af
 	ld [hl], a
+											; first time is to check collisions and move the
+	call UpdateSprites			 			; player if needed.  We load all the data for
+	call LoadScreenRelatedData	 			; CollisionCheckAfterVersionChange to check against
+	call CollisionCheckAfterVersionChange	; and then we do it again
 	
+	call ResetMapVariables 		 ; round 2 is for showing the map, if we don't do this
+	call LoadTextBoxTilePatterns ; twice sprites will maintain their poximity to the
+	call LoadMapHeader			 ; player and then snap into position when the menu is
+	call ClearVariable			 ; closed and frankly I don't like the way that looks,
+	call InitMapSprites			 ; so we just do this stuff twice
 	call LoadFontTilePatterns
-	call LoadScreenRelatedData
-	call CollisionCheckAfterVersionChange
+	
 	call LoadCurrentMapView
 	call CopyMapViewToVRAM
 	call EnableLCD
