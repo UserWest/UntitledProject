7560c9b116ccc8944be448a1942044500e111561
UserWest <WestofMystic@gmail.com>
2021-11-05 19:56:46 -0400

    Use table instead of several checks

--- data/maps/map_header_pointers.asm
+++ data/maps/map_header_pointers.asm
@@ -252,85 +252,268 @@ MapHeaderPointers::
 	dw SummerBeachHouse_h
 	assert_table_length NUM_MAPS
 
-ChangedMapHeaderPointers::
-	table_width 2, ChangedMapHeaderPointers
+MapHeaderPointersRB::
+	table_width 2, MapHeaderPointersRB
+	dw PalletTown_h
+	dw ViridianCity_h
+	dw PewterCity_h
+	dw CeruleanCity_h
+	dw LavenderTown_h
+	dw VermilionCity_h
+	dw CeladonCity_h
+	dw FuchsiaCity_h
+	dw CinnabarIsland_h
+	dw IndigoPlateau_h
+	dw SaffronCity_h
+	dw SaffronCity_h ; UNUSED_MAP_0B
+	dw Route1_h
+	dw Route2_h
+	dw Route3_h
+	dw Route4RB_h
+	dw Route5_h
+	dw Route6_h
+	dw Route7_h
+	dw Route8_h
+	dw Route9_h
+	dw Route10_h
+	dw Route11_h
+	dw Route12_h
+	dw Route13_h
+	dw Route14_h
+	dw Route15_h
+	dw Route16_h
+	dw Route17_h
+	dw Route18_h
+	dw Route19RB_h
+	dw Route20_h
+	dw Route21_h
+	dw Route22_h
+	dw Route23_h
+	dw Route24_h
+	dw Route25_h
+	dw RedsHouse1F_h
+	dw RedsHouse2F_h
+	dw BluesHouse_h
 	dw OaksLabRB_h
+	dw ViridianPokecenter_h
+	dw ViridianMart_h
+	dw ViridianSchoolHouse_h
+	dw ViridianNicknameHouse_h
+	dw ViridianGym_h
+	dw DiglettsCaveRoute2_h
+	dw ViridianForestNorthGate_h
+	dw Route2TradeHouse_h
+	dw Route2Gate_h
+	dw ViridianForestSouthGate_h
+	dw ViridianForest_h
+	dw Museum1F_h
+	dw Museum2F_h
+	dw PewterGym_h
+	dw PewterNidoranHouse_h
+	dw PewterMart_h
+	dw PewterSpeechHouse_h
+	dw PewterPokecenter_h
+	dw MtMoon1F_h
+	dw MtMoonB1F_h
+	dw MtMoonB2F_h
+	dw CeruleanTrashedHouse_h
+	dw CeruleanMelaniesHouse_h
+	dw CeruleanPokecenter_h
+	dw CeruleanGym_h
+	dw BikeShop_h
+	dw CeruleanMart_h
+	dw MtMoonPokecenter_h
+	dw CeruleanTrashedHouse_h ; CERULEAN_TRASHED_HOUSE_COPY
+	dw Route5Gate_h
+	dw UndergroundPathRoute5_h
+	dw Daycare_h
+	dw Route6Gate_h
+	dw UndergroundPathRoute6_h
+	dw UndergroundPathRoute6_h ; UNDERGROUND_PATH_ROUTE_6_COPY
+	dw Route7Gate_h
+	dw UndergroundPathRoute7_h
+	dw UndergroundPathRoute7Copy_h
+	dw Route8Gate_h
+	dw UndergroundPathRoute8_h
+	dw RockTunnelPokecenter_h
+	dw RockTunnel1F_h
+	dw PowerPlant_h
+	dw Route11Gate1F_h
+	dw DiglettsCaveRoute11_h
+	dw Route11Gate2F_h
+	dw Route12Gate1F_h
+	dw BillsHouse_h
+	dw VermilionPokecenter_h
+	dw PokemonFanClub_h
+	dw VermilionMart_h
+	dw VermilionGym_h
+	dw VermilionPidgeyHouse_h
+	dw VermilionDock_h
+	dw SSAnne1F_h
+	dw SSAnne2F_h
+	dw SSAnne3F_h
+	dw SSAnneB1F_h
+	dw SSAnneBow_h
+	dw SSAnneKitchen_h
+	dw SSAnneCaptainsRoom_h
+	dw SSAnne1FRooms_h
+	dw SSAnne2FRooms_h
+	dw SSAnneB1FRooms_h
+	dw LancesRoom_h ; UNUSED_MAP_69
+	dw LancesRoom_h ; UNUSED_MAP_6A
+	dw LancesRoom_h ; UNUSED_MAP_6B
+	dw VictoryRoad1F_h
+	dw LancesRoom_h ; UNUSED_MAP_6D
+	dw LancesRoom_h ; UNUSED_MAP_6E
+	dw LancesRoom_h ; UNUSED_MAP_6F
+	dw LancesRoom_h ; UNUSED_MAP_70
+	dw LancesRoom_h
+	dw LancesRoom_h ; UNUSED_MAP_72
+	dw LancesRoom_h ; UNUSED_MAP_73
+	dw LancesRoom_h ; UNUSED_MAP_74
+	dw LancesRoom_h ; UNUSED_MAP_75
+	dw HallOfFame_h
+	dw UndergroundPathNorthSouth_h
+	dw ChampionsRoom_h
+	dw UndergroundPathWestEast_h
+	dw CeladonMart1F_h
+	dw CeladonMart2F_h
+	dw CeladonMart3F_h
+	dw CeladonMart4F_h
+	dw CeladonMartRoof_h
+	dw CeladonMartElevator_h
+	dw CeladonMansion1F_h
+	dw CeladonMansion2F_h
+	dw CeladonMansion3F_h
+	dw CeladonMansionRoof_h
+	dw CeladonMansionRoofHouse_h
+	dw CeladonPokecenter_h
 	dw CeladonGymRB_h
 	dw GameCornerRB_h
-	dw Route4RB_h
-	dw Route19RB_h
-	dw SummerBeachHouseRB_h
-	dw CeruleanCave1FRB_h
+	dw CeladonMart5F_h
+	dw GameCornerPrizeRoom_h
+	dw CeladonDiner_h
+	dw CeladonChiefHouse_h
+	dw CeladonHotel_h
+	dw LavenderPokecenter_h
+	dw PokemonTower1F_h
+	dw PokemonTower2F_h
+	dw PokemonTower3F_h
+	dw PokemonTower4F_h
+	dw PokemonTower5F_h
+	dw PokemonTower6F_h
+	dw PokemonTower7F_h
+	dw MrFujisHouse_h
+	dw LavenderMart_h
+	dw LavenderCuboneHouse_h
+	dw FuchsiaMart_h
+	dw FuchsiaBillsGrandpasHouse_h
+	dw FuchsiaPokecenter_h
+	dw WardensHouse_h
+	dw SafariZoneGate_h
+	dw FuchsiaGym_h
+	dw FuchsiaMeetingRoom_h
+	dw SeafoamIslandsB1F_h
+	dw SeafoamIslandsB2F_h
+	dw SeafoamIslandsB3F_h
+	dw SeafoamIslandsB4F_h
+	dw VermilionOldRodHouse_h
+	dw FuchsiaGoodRodHouse_h
+	dw PokemonMansion1F_h
+	dw CinnabarGym_h
+	dw CinnabarLab_h
+	dw CinnabarLabTradeRoom_h
+	dw CinnabarLabMetronomeRoom_h
+	dw CinnabarLabFossilRoom_h
+	dw CinnabarPokecenter_h
+	dw CinnabarMart_h
+	dw CinnabarMart_h ; CINNABAR_MART_COPY
+	dw IndigoPlateauLobby_h
+	dw CopycatsHouse1F_h
+	dw CopycatsHouse2F_h
+	dw FightingDojo_h
+	dw SaffronGym_h
+	dw SaffronPidgeyHouse_h
+	dw SaffronMart_h
+	dw SilphCo1F_h
+	dw SaffronPokecenter_h
+	dw MrPsychicsHouse_h
+	dw Route15Gate1F_h
+	dw Route15Gate2F_h
+	dw Route16Gate1F_h
+	dw Route16Gate2F_h
+	dw Route16FlyHouse_h
+	dw Route12SuperRodHouse_h
+	dw Route18Gate1F_h
+	dw Route18Gate2F_h
+	dw SeafoamIslands1F_h
+	dw Route22Gate_h
+	dw VictoryRoad2F_h
+	dw Route12Gate2F_h
+	dw VermilionTradeHouse_h
+	dw DiglettsCave_h
+	dw VictoryRoad3F_h
+	dw RocketHideoutB1F_h
+	dw RocketHideoutB2F_h
+	dw RocketHideoutB3F_h
+	dw RocketHideoutB4F_h
+	dw RocketHideoutElevator_h
+	dw RocketHideoutElevator_h ; UNUSED_MAP_CC
+	dw RocketHideoutElevator_h ; UNUSED_MAP_CD
+	dw RocketHideoutElevator_h ; UNUSED_MAP_CE
+	dw SilphCo2F_h
+	dw SilphCo3F_h
+	dw SilphCo4F_h
+	dw SilphCo5F_h
+	dw SilphCo6F_h
+	dw SilphCo7F_h
+	dw SilphCo8F_h
+	dw PokemonMansion2F_h
+	dw PokemonMansion3F_h
+	dw PokemonMansionB1F_h
+	dw SafariZoneEast_h
+	dw SafariZoneNorth_h
+	dw SafariZoneWest_h
+	dw SafariZoneCenter_h
+	dw SafariZoneCenterRestHouse_h
+	dw SafariZoneSecretHouse_h
+	dw SafariZoneWestRestHouse_h
+	dw SafariZoneEastRestHouse_h
+	dw SafariZoneNorthRestHouse_h
 	dw CeruleanCave2FRB_h
 	dw CeruleanCaveB1FRB_h
-	assert_table_length 9
+	dw CeruleanCave1FRB_h
+	dw NameRatersHouse_h
+	dw CeruleanBadgeHouse_h
+	dw Route16Gate1F_h ; UNUSED_MAP_E7
+	dw RockTunnelB1F_h
+	dw SilphCo9F_h
+	dw SilphCo10F_h
+	dw SilphCo11F_h
+	dw SilphCoElevator_h
+	dw SilphCo2F_h ; UNUSED_MAP_ED
+	dw SilphCo2F_h ; UNUSED_MAP_EE
+	dw TradeCenter_h
+	dw Colosseum_h
+	dw SilphCo2F_h ; UNUSED_MAP_F1
+	dw SilphCo2F_h ; UNUSED_MAP_F2
+	dw SilphCo2F_h ; UNUSED_MAP_F3
+	dw SilphCo2F_h ; UNUSED_MAP_F4
+	dw LoreleisRoom_h
+	dw BrunosRoom_h
+	dw AgathasRoom_h
+	dw SummerBeachHouseRB_h
+	assert_table_length NUM_MAPS
 	
-CheckForVersionMapDifferences::
-	cp VERMILION_DOCK
-	jr z, .VermilionDock
-	call CheckForYellowVersion
-	jp z, .CheckForCeruleanCaveYellow
+CheckForChangingMap::
 	ld a, [wCurMap]
-	cp OAKS_LAB
-	jr nz, .notOaksLab
-	ld e, 0
-	jr .foundMap
-.notOaksLab
-	cp CELADON_GYM
-	jr nz, .notCeladonGym
-	ld e, 1
-	jr .foundMap
-.notCeladonGym
-	cp GAME_CORNER
-	jr nz, .notGameCorner
-	ld e, 2
-	jr .foundMap
-.notGameCorner
-	cp ROUTE_4
-	jr nz, .notRoute4
-	ld e, 3
-	jr .foundMap
-.notRoute4
-	cp ROUTE_19
-	jr nz, .notRoute19
-	ld e, 4
-	jr .foundMap
-.notRoute19
-	cp SUMMER_BEACH_HOUSE
-	jr nz, .notSummerBeachHouse
-	ld e, 5
-	jr .foundMap
-.notSummerBeachHouse
 	cp CERULEAN_CAVE_1F
-	jr nz, .notCeruleanCave1F
-	ld e, 6
-	jr .foundCeruleanCave
-.notCeruleanCave1F
+	jr z, .foundCeruleanCave
 	cp CERULEAN_CAVE_2F
-	jr nz, .notCeruleanCave2F
-	ld e, 7
-	jr .foundCeruleanCave
-.notCeruleanCave2F
+	jr z, .foundCeruleanCave
 	cp CERULEAN_CAVE_B1F
-	ret nz
-	ld e, 8
+	jr z, .foundCeruleanCave
+	ret
 .foundCeruleanCave
-	call ClearVariable ; wipes wUniversalVariable to allow player to spawn at last warp
-.foundMap
-	ld hl, ChangedMapHeaderPointers
-	ld d, 0
-	ret
-.VermilionDock
-	; haven't done yet
-	ret
-.CheckForCeruleanCaveYellow
-	cp CERULEAN_CAVE_1F
-	jr z, .foundCeruleanCaveYellow
-	cp CERULEAN_CAVE_2F
-	jr z, .foundCeruleanCaveYellow
-	cp CERULEAN_CAVE_B1F
-	jr z, .foundCeruleanCaveYellow
-	ret
-.foundCeruleanCaveYellow
 	call ClearVariable ; wipes wUniversalVariable to allow player to spawn at last warp
 	ret
